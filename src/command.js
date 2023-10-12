a1f4ec30c5865ad820ab102f7b59cb3f71cae21e
kushal <kushalbhetuwal@gmail.com>
2023-10-12 15:15:51 +1100

    updated the command

--- src/command.js
+++ src/command.js
@@ -1,73 +1,53 @@
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import yargs, { positional } from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
 yargs(hideBin(process.argv))
-  .command(
-    "new <note>",
-    "create a new note",
-    (yargs) => {
-      return yargs.positional("note", {
-        describe: "The content of the note you want to create",
-        type: "string",
-      });
-    },
-    async (argv) => {
-      console.info(argv.note);
-    },
-  )
-  .option("tags", {
-    alias: "t",
-    type: "string",
-    description: "tags to add to the note",
-  })
-  .command(
-    "all",
-    "get all notes",
-    () => {},
-    async (argv) => {},
-  )
-  .command(
-    "find <filter>",
-    "get matching notes",
-    (yargs) => {
-      return yargs.positional("filter", {
-        describe:
-          "The search term to filter notes by, will be applied to note.content",
-        type: "string",
-      });
-    },
-    async (argv) => {
-      console.info(argv.filter);
-    },
-  )
-  .command(
-    "remove <id>",
-    "remove a note by id",
-    (yargs) => {
-      return yargs.positional("id", {
-        type: "number",
-        description: "The id of the note you want to remove",
-      });
-    },
-    async (argv) => {},
-  )
-  .command(
-    "web [port]",
-    "launch website to see notes",
-    (yargs) => {
-      return yargs.positional("port", {
-        describe: "port to bind on",
-        default: 5000,
-        type: "number",
-      });
-    },
-    async (argv) => {},
-  )
-  .command(
-    "clean",
-    "remove all notes",
-    () => {},
-    async (argv) => {},
-  )
+  .command("new <note>", "fetch the content of the app kushal", yargs=>{
+    return yargs.positional('note',{
+      type:'string',
+      description:'the content of the note to create',
+    })
+  }, (argv)=>console.log(argv))
+   .option('t', {
+    alias:'t',
+    type:'string',
+    description:'tags to add the note'
+   })
+   .command('all', "get all notes", ()=>{}, (arg)=>{
+
+   })
+   .command("find <filter>", "get the matching notes", (yargs)=>{
+    return yargs.positional('filter',{
+      describe:"The search term to filter notes will be applied to note.content",
+      type:'string',
+    })
+   }, (argv)=>{
+
+   })
+   .command('remove <id>', "remove a note by id", yargs=>{
+    return yargs.positional('id',{
+      describe:"the id that you want to remove",
+      type:'number'
+    })
+   }, argv=>{
+
+   })
+   .command('web [port]', "launch a website to see notes", (yargs)=>{
+    return positional('port', {
+      describe:'port to bind on',
+      type:'number',
+      default:5000
+    })
+   }, argv=>{
+
+   })
+   .command('clean', 'remove all notes', (yargs)=>{
+    return yargs.positional('clean', {
+      describe:"Clean all notes",
+      type:'string'
+    })
+   }, argv=>{
+
+   })
   .demandCommand(1)
-  .parse();
+  .parse()// you can execute now:
